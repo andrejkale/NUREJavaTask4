ae99b448edba59a3589e914a1990ebc2041cefbb
Andrii Kalie <andrejkale@gmail.com>
2022-12-21 21:18:49 +0200

    Initial commit

---
+++ src/main/java/Circle.java
@@ -0,0 +1,6 @@
+public class Circle extends Point {
+    @Override
+    public void sizing() {
+
+    }
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,6 @@
+public abstract class Point {
+    public int x;
+    public int y;
+
+    public abstract void sizing();
+}
